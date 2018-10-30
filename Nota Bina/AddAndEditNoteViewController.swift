5e845b61c41e5abd241ecdf1d1aad234a2653a81
MahmoudAmin39 <dr.mahmoud39@gmail.com>
2018-10-30 21:09:14 +0200

    UI changes concerning picking the style and color of the text

--- Nota Bina/AddAndEditNoteViewController.swift
+++ Nota Bina/AddAndEditNoteViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class AddAndEditNoteViewController: UIViewController {
     
+    @IBOutlet weak var noteBodyLabel: UITextView!
+    
+    var attributes = [NSAttributedString.Key : Any]() {
+        didSet {
+            let attributedText = NSAttributedString(string: noteBodyLabel.text, attributes: attributes)
+            noteBodyLabel.attributedText = attributedText
+        }
+    }
+    
+    var textColor: UIColor? {
+        didSet {
+            attributes[.foregroundColor] = textColor
+        }
+    }
+    
+    var textFontStyle: UIFont? {
+        didSet {
+            attributes[.font] = textFontStyle
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
     
     @IBAction func saveNoteAndDismiss(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
+
+    @IBAction func colorClicked(_ button: UIButton) {
+        
+        switch button.titleLabel?.text {
+        case "Red": textColor = UIColor.red
+        case "Blue": textColor = UIColor.blue
+        case "Green": textColor = UIColor.green
+        case "Orange": textColor = UIColor.orange
+        default: textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func styleClicked(_ button: UIButton) {
+        switch button.titleLabel?.text {
+        case "Bold": textFontStyle = UIFont.boldSystemFont(ofSize: 18)
+        case "Italic": textFontStyle = UIFont.italicSystemFont(ofSize: 18)
+        default: textFontStyle = UIFont.preferredFont(forTextStyle: .title3)
+        }
+    }
+    
 }
